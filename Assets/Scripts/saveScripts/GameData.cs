b2cc7a6b480c084f51ebace030fac647e2691f5b
BiggestGuz <Diblopro@gmail.com>
2025-01-04 17:31:05 +0100

    Add save, load and delete health

--- Assets/Scripts/saveScripts/GameData.cs
+++ Assets/Scripts/saveScripts/GameData.cs
@@ -11,7 +11,6 @@ public class GameData
     public int totEnemiesKilled;
     public int enemiesSpawned;
     public int enemiesDefeated;
-    public float playerHealth;
 
     // Default constructor initializes fields with default values
     public GameData()
@@ -24,6 +23,5 @@ public class GameData
         this.totEnemiesKilled = 0;
         this.enemiesSpawned = 0;
         this.enemiesDefeated = 0;
-        this.playerHealth = 100f;
     }
 }
